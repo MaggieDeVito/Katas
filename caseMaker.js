9bd1bf3d576a09566397b4dc3995096c903264a5
MaggieDeVito <maggiemaydevito@gmail.com>
2021-06-18 22:35:16 +0000

    final commit

--- caseMaker.js
+++ caseMaker.js
@@ -1,5 +1,6 @@
 const makeCase = function (input, type) {
   let arr = input.split(" ");
+
   if (type === "camel") {
     for (let i = 0; i < arr.length; i++) {
       if (i !== 0) {
@@ -57,12 +58,3 @@ const makeCase = function (input, type) {
     return string;
   }
 };
-
-console.log(makeCase("this is a string", "camel"));
-console.log(makeCase("this is a string", "pascal"));
-console.log(makeCase("this is a string", "snake"));
-console.log(makeCase("this is a string", "kebab"));
-console.log(makeCase("this is a string", "title"));
-console.log(makeCase("this is a string", "vowel"));
-console.log(makeCase("this is a string", "consonant"));
-// console.log(makeCase("this is a string", ["upper", "snake"]);
